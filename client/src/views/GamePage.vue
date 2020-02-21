bb66872d3e88bad8a6c4e61c8447e1fdb47dfa10
Mazda94 <m.alli9401@gmail.com>
2020-02-21 14:53:41 +0700

    add music

--- client/src/views/GamePage.vue
+++ client/src/views/GamePage.vue
@@ -10,12 +10,7 @@
             <player :player="player" v-for="(player,i) in players" :key="i" />
           </div>
           <!-- <div class="row" v-if="!rooms[1]"><h1>Player is belum cukup...</h1></div> -->
-          <button
-            class="btn btn-warning btn-lg text-white w-25 mt-3"
-            @click="getQuotes"
-          >
-            Mulai
-          </button>
+          <button class="btn btn-warning btn-lg text-white w-25 mt-3" @click="getQuotes">Mulai</button>
         </div>
       </div>
       <div v-else class="d-flex justify-content-center flex-column">
@@ -26,26 +21,20 @@
           class="p-4 bg-light text-center rounded-pill"
           onmousedown="return false"
           onselectstart="return false"
-        >
-          {{ sentence }}
-        </h1>
+        >{{ sentence }}</h1>
         <br />
         <br />
-        <p class="text-center text-white" style="font-size: 72px;">29</p>
+        <p class="text-center text-white" style="font-size: 72px;">your score : {{ score }}</p>
 
         <!-- <form id="form-join" class="d-flex flex-column"> -->
-          <input
-            type="text"
-            v-on:keyup="typeMonitor"
-            v-model="answer"
-            class="input-form"
-            placeholder="Type here"
-          />
+        <input
+          type="text"
+          v-on:keyup="typeMonitor"
+          v-model="answer"
+          class="input-form"
+          placeholder="Type here"
+        />
         <!-- </form> -->
-
-        <p class="text-center text-white" style="font-size: 72px;">
-          your score : {{ score }}
-        </p>
       </div>
     </div>
   </div>
@@ -71,9 +60,9 @@ export default {
   },
   created() {
     socket.on("next_sentence", () => {
-      this.randomize();      
+      this.randomize();
     });
-    socket.on('game_start', () => {
+    socket.on("game_start", () => {
       this.$axios
         .get("https://programming-quotes-api.herokuapp.com/quotes/lang/en")
         .then(({ data }) => {
@@ -83,7 +72,7 @@ export default {
         .catch(err => {
           console.log(err);
         });
-    })
+    });
   },
   methods: {
     typeMonitor() {
@@ -93,12 +82,21 @@ export default {
         this.score += 10;
         if (this.score == 80) {
           console.log("game selesai");
-        }else{
+        } else {
           localStorage.setItem("score", this.score);
         }
       }
     },
+    playSound(sound) {
+      if (sound) {
+        var audio = new Audio(sound);
+        audio.play();
+      }
+    },
     randomize() {
+      this.playSound(
+        "http://soundbible.com/mp3/Air Plane Ding-SoundBible.com-496729130.mp3"
+      );
       this.quotes = this.quotes.filter(a => {
         return a.en.length < 50;
       });
@@ -112,7 +110,7 @@ export default {
         .get("/rooms")
         .then(({ data }) => {
           let playerData = JSON.parse(localStorage.player);
-          this.players = data.rooms[playerData.RoomId-1].Players;
+          this.players = data.rooms[playerData.RoomId - 1].Players;
           console.log(this.players);
         })
         .catch(err => {
@@ -121,18 +119,12 @@ export default {
     },
     getQuotes() {
       socket.emit("game_start");
-    },
+    }
   },
   mounted() {
     this.getRooms();
-    // this.quotes = this.getQuotes;
-    // this.randomize();
+    new Audio('/spongbob.mp3').play();
   }
-  // computed: {
-  //   getQuotes() {
-  //     return this.$store.state.quotes;
-  //   }
-  // }
 };
 </script>
 
